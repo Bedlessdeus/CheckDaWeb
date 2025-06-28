752dab37a0186ca12ea5e1d546b7d0c118c876a8
Joseph Perthold <Bedlesssgod@gmail.com>
2025-06-28 03:04:06 +0200

    CSP Variable
    + Added Required Base URL variable for generating CSP

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,29 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { env } from 'process';
+
+console.log(env.SELF_URL);
 
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		csp: {
 			directives: {
-				'default-src': ["'self'"],
-				'script-src': ["'self'"],
-				'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
-				'img-src': ["'self'", 'data:', 'blob:'],
-				'font-src': ["'self'", 'https://fonts.gstatic.com'],
-				'connect-src': ["'self'"]
+				'default-src': ["'self'", env.PRIVATE_SELF_URL ?? 'http://localhost:5173'],
+				'script-src': ["'self'", env.PRIVATE_SELF_URL ?? 'http://localhost:5173'],
+				'style-src': [
+					"'self'",
+					env.PRIVATE_SELF_URL ?? 'http://localhost:5173',
+					"'unsafe-inline'",
+					'https://fonts.googleapis.com'
+				],
+				'img-src': ["'self'", env.PRIVATE_SELF_URL ?? 'http://localhost:5173', 'data:', 'blob:'],
+				'font-src': [
+					"'self'",
+					env.PRIVATE_SELF_URL ?? 'http://localhost:5173',
+					'https://fonts.gstatic.com'
+				],
+				'connect-src': ["'self'", env.PRIVATE_SELF_URL ?? 'http://localhost:5173']
 			}
 		},
 		adapter: adapter()
